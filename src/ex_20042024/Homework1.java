9827ac4e01b45e2416d56108545daa19914a880a
eerthiram <keerthiram484@gmail.com>
2024-05-16 13:33:34 +0530

    20 APR 2024

---
+++ src/ex_20042024/Homework1.java
@@ -0,0 +1,12 @@
+package ex_20042024;
+
+public class Homework {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(++a + a++ + a++);
+        //++a-> 1+10=11 a=11
+        //a++-> a=11  11+1=12
+        //a++-> a==12
+        //11+12+12
+    }
+}
