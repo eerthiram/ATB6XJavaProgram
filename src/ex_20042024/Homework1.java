273c179a63a9778c0d53073a1e41c48896457794
Keerthanaram <keerthiram484@gmail.com>
2024-05-16 13:34:46 +0530

    Update Homework1.java

--- src/ex_20042024/Homework1.java
+++ src/ex_20042024/Homework1.java
@@ -4,9 +4,6 @@ public class Homework {
     public static void main(String[] args) {
         int a=10;
         System.out.println(++a + a++ + a++);
-        //++a-> 1+10=11 a=11
-        //a++-> a=11  11+1=12
-        //a++-> a==12
-        //11+12+12
+        
     }
 }
